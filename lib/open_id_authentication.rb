5813e28df1692b844ab41a54fc4f18c1d8c68578
rick <rick@5ecf4fe2-1ee6-0310-87b1-e25e094e27de>
2007-02-27 05:48:39 +0000

    add sreg support
    git-svn-id: http://svn.rubyonrails.org/rails/plugins/open_id_authentication@6252 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- lib/open_id_authentication.rb
+++ lib/open_id_authentication.rb
@@ -5,23 +5,24 @@ module OpenIdAuthentication
       (Object.const_defined?(:OpenID) && user_name =~ /^https?:\/\//i) || params[:open_id_complete]
     end
 
-    def authenticate_with_open_id(identity_url, &block) #:doc:
+    def authenticate_with_open_id(identity_url, fields = {}, &block) #:doc:
       if params[:open_id_complete].nil?
-        begin_open_id_authentication(identity_url, &block)
+        begin_open_id_authentication(identity_url, fields, &block)
       else
         complete_open_id_authentication(&block)
       end
     end
 
-
   private
-    def begin_open_id_authentication(identity_url)
+    def begin_open_id_authentication(identity_url, fields = {})
       open_id_response = open_id_consumer.begin(identity_url)
 
       case open_id_response.status
       when OpenID::FAILURE
-        yield :missing, identity_url
+        yield :missing, identity_url, nil
       when OpenID::SUCCESS
+        open_id_response.add_extension_arg('sreg','required', [fields[:required]].flatten * ',') if fields[:required]
+        open_id_response.add_extension_arg('sreg','optional', [fields[:optional]].flatten * ',') if fields[:optional]
         redirect_to(open_id_response.redirect_url(
           root_url, open_id_response.return_to(
             "#{request.protocol + request.host_with_port + request.request_uri}?open_id_complete=1"
@@ -35,12 +36,12 @@ module OpenIdAuthentication
 
       case open_id_response.status
       when OpenID::CANCEL
-        yield :canceled, open_id_response.identity_url
+        yield :canceled, open_id_response.identity_url, nil
       when OpenID::FAILURE
         logger.info "OpenID authentication failed: #{open_id_response.msg}"
-        yield :failed, open_id_response.identity_url
+        yield :failed, open_id_response.identity_url, nil
       when OpenID::SUCCESS
-        yield :successful, open_id_response.identity_url
+        yield :successful, open_id_response.identity_url, open_id_response.extension_response('sreg')
       end      
     end
 
