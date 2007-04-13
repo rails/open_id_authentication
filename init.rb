cd5973dcbfc61c2576e916a709ac1bab19924b36
david <david@5ecf4fe2-1ee6-0310-87b1-e25e094e27de>
2007-04-13 02:09:24 +0000

    Should have removed install.rb, not init.rb
    git-svn-id: http://svn.rubyonrails.org/rails/plugins/open_id_authentication@6520 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

---
+++ init.rb
@@ -0,0 +1,8 @@
+begin
+  gem 'ruby-openid'
+  require 'openid'
+rescue LoadError
+  puts "Install the ruby-openid gem to enable OpenID support"
+end
+
+ActionController::Base.send :include, OpenIdAuthentication
--- install.rb
+++
@@ -1,4 +0,0 @@
-puts "Creating tmp/openids..."
-tmp_path = RAILS_ROOT + "/tmp/openids"
-Dir.mkdir(tmp_path) unless File.exist?(tmp_path)
-puts "This directory should be hosted on a shared drive for all application servers"
