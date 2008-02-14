ad725e0a86a93b8c35f42f542307bb8faaa60316
david <david@5ecf4fe2-1ee6-0310-87b1-e25e094e27de>
2008-02-14 22:57:33 +0000

    Tied plugin to ruby-openid 1.1.4 gem until we can make it compatible with 2.x [DHH]
    git-svn-id: http://svn.rubyonrails.org/rails/plugins/open_id_authentication@8872 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- init.rb
+++ init.rb
@@ -1,12 +1,8 @@
 begin
+  gem 'ruby-openid', '=1.1.4'
   require 'openid'  
 rescue LoadError
-  begin
-    gem 'ruby-openid'
-    require 'openid'
-  rescue LoadError
-    puts "Install the ruby-openid gem to enable OpenID support"
-  end
+  puts "Install the ruby-openid gem to enable OpenID support"
 end
 
 ActionController::Base.send :include, OpenIdAuthentication
