da282f1dac9219d0bb8167d0a0baf6e5acf2af90
rick <rick@5ecf4fe2-1ee6-0310-87b1-e25e094e27de>
2008-04-02 03:33:33 +0000

    delete the format parameter so that openid works on the iphone.  add support for config.gem on rails 2.1+
    git-svn-id: http://svn.rubyonrails.org/rails/plugins/open_id_authentication@9207 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- init.rb
+++ init.rb
@@ -1,11 +1,17 @@
-begin
-  require 'openid'
-rescue LoadError
+if config.respond_to?(:gems)
+  config.gem 'ruby-openid', :lib => 'openid', :version => '>=2.0.4'
+else
   begin
-    gem 'ruby-openid', '>=2.0.4'
-  rescue Gem::LoadError
-    puts "Install the ruby-openid gem to enable OpenID support"
+    require 'openid'
+  rescue LoadError
+    begin
+      gem 'ruby-openid', '>=2.0.4'
+    rescue Gem::LoadError
+      puts "Install the ruby-openid gem to enable OpenID support"
+    end
   end
 end
 
-ActionController::Base.send :include, OpenIdAuthentication
+config.to_prepare do
+  ActionController::Base.send :include, OpenIdAuthentication
+end
--- lib/open_id_authentication.rb
+++ lib/open_id_authentication.rb
@@ -101,6 +101,7 @@ module OpenIdAuthentication
 
     def complete_open_id_authentication
       params_with_path = params.reject { |key, value| request.path_parameters[key] }
+      params_with_path.delete(:format)
       open_id_response = timeout_protection_from_identity_server { open_id_consumer.complete(params_with_path, requested_url) }
       identity_url     = normalize_url(open_id_response.endpoint.claimed_id) if open_id_response.endpoint.claimed_id
 
