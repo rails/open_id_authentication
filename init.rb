95753785a5dba949e226f2161e51caca76167d71
Joshua Peek <josh@joshpeek.com>
2010-01-18 09:22:52 -0600

    rack-openid already depends on ruby-openid

--- init.rb
+++ init.rb
@@ -1,6 +1,5 @@
 if Rails.version < '3'
   config.gem 'rack-openid', :lib => 'rack/openid', :version => '>=0.2.1'
-  config.gem 'ruby-openid', :lib => 'openid', :version => '>=2.1.7'
 end
 
 config.middleware.use :OpenIdAuthentication
